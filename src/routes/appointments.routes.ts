773e3b458d737c30d134523d8060fe7deb75656d
jamyllemagal <jamyllemagal@gmail.com>
2020-08-23 20:17:34 -0300

    Include appointment and user relationship

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -15,13 +15,13 @@ appointmentsRouter.get('/', async (request, response) => {
 
 appointmentsRouter.post('/', async (request, response) => {
     try {
-        const { provider, date } = request.body;
+        const { provider_id, date } = request.body;
     
         const parsedDate = parseISO(date);
 
         const createAppointment = new CreateAppointmentService();
 
-        const appointment = await createAppointment.execute({ date: parsedDate, provider })
+        const appointment = await createAppointment.execute({ date: parsedDate, provider_id })
 
         return response.json(appointment);
     } catch (error) {
--- src/services/CreateAppointmentService.ts
+++ src/services/CreateAppointmentService.ts
@@ -5,11 +5,11 @@ import { getCustomRepository } from 'typeorm';
 
 interface Request {
     date: Date;
-    provider: string;
+    provider_id: string;
 };
 
 class CreateAppointmentService {
-    public async execute({ date, provider }: Request): Promise<Appointment> {
+    public async execute({ date, provider_id }: Request): Promise<Appointment> {
         const appointmentsRepository = getCustomRepository(AppointmentsRepository);
         const appointmentDate = startOfHour(date);
 
@@ -17,7 +17,7 @@ class CreateAppointmentService {
             throw Error("This appointment is already booked.");
         };
 
-        const appointment = appointmentsRepository.create({provider, date: appointmentDate});
+        const appointment = appointmentsRepository.create({ provider_id, date: appointmentDate });
 
         await appointmentsRepository.save(appointment);
 
