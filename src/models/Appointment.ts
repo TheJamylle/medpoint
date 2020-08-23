e5002e5f3288aa4332ad71f679e1e035078f1e62
jamyllemagal <jamyllemagal@gmail.com>
2020-08-23 19:32:49 -0300

    Model Agendamento

--- src/models/Appointment.ts
+++ src/models/Appointment.ts
@@ -1,4 +1,5 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { Entity, Column, PrimaryGeneratedColumn, ManyToOne, JoinColumn } from 'typeorm';
+import User from './User';
 
 @Entity('appointments')
 class Appointment {
@@ -6,7 +7,11 @@ class Appointment {
     id: string;
 
     @Column()
-    provider: string;
+    provider_id: string;
+
+    @ManyToOne(() => User)
+    @JoinColumn({ name: 'provider_id' })
+    provider: User;
 
     @Column('timestamp with time zone')
     date: Date;
